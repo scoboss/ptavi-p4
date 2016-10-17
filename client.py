69862a4ed5198b092f36e3afcfff050afe1c6db3
Sandra Cobos Sacido <sandra.12@outlook.es>
2016-10-17 10:18:27 +0200

    Comienzo practica

--- client.py
+++ client.py
@@ -3,7 +3,7 @@
 """
 Programa cliente UDP que abre un socket a un servidor
 """
-
+# Importa el modulo socket.
 import socket
 
 # Constantes. Dirección IP del servidor y contenido a enviar
@@ -18,8 +18,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
+# Send para enviar y recv para recibir. Ademas el buffer de recv en bytes.
 
-# El socket deja de existir cuando acaba el with.
+# El socket deja de existir cuando acaba el with no hace falta cerrarlo.
 # b linea 18: bytes, si fuera u seria en utf.
 
 print("Socket terminado.")
