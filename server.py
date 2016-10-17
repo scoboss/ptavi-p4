55f15f5f57259c8bdca351c7fb1d176321c200f5
Sandra Cobos Sacido <sandra.12@outlook.es>
2016-10-17 10:22:45 +0200

    Ejercicio 3

--- server.py
+++ server.py
@@ -3,20 +3,20 @@
 """
 Clase (y programa principal) para un servidor de eco en UDP simple
 """
-
+# Importa el modulo socketserver.
 import socketserver
 
-
+#Claase que manejara las peticiones. Clase hereda de DatagramaRequestHandler.
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-
+    # Metodo que se ejecuta cada vez que recibimos una peticion.
     def handle(self):
         self.wfile.write(b"Hemos recibido tu peticion")
         for line in self.rfile:
             print("El cliente nos manda ", line.decode('utf-8'))
-#Tratamos como un fichero.
+#Tratamos como un fichero; self.wfile y self.rfile.
 
 if __name__ == "__main__":
     serv = socketserver.UDPServer(('', 6001), EchoHandler)
