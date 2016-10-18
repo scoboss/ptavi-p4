3f1ff7c1e9b1a9b7669ac1d8b1f3b8d228d80413
Sandra Cobos Sacido <sandra.12@outlook.es>
2016-10-18 10:41:16 +0200

    Modificamos cliente. Ejercicio 6.

--- client.py
+++ client.py
@@ -11,13 +11,16 @@ import sys
 # Constantes. Dirección IP del servidor y contenido a enviar.
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
-LINE_LISTA = sys.argv[2:]
+LINE_LISTA = sys.argv[3:]
 LINE = ' '.join(LINE_LISTA)
 
+if (LINE.split()[0] == 'register'):
+        LINE = 'REGISTER' + 'sip' + LINE.split()[1] + 'SIP/2.0\r\n\r\n'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto.
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((SERVER, PORT))
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+my_socket.connect((SERVER, PORT))
     print("Enviando:", LINE)
     my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
